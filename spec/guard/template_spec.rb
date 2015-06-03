b3020f99d1f7815b37cb3aeaee76e4f3b89afd0c
Cezary Baginski <cezary@chronomantic.net>
2015-06-03 22:53:56 +0200

    fix Guardfile template to be usable

--- spec/guard/template_spec.rb
+++ spec/guard/template_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe "Guard::Yield" do
   describe "template" do
     subject { Guard::Compat::Test::Template.new("Guard::Yield") }
 
+    # Just validate enough to avoid syntax errors
     it "set an example watcher" do
-      expect(subject.changed("foo.rb")).to eq(%w(bar.rb))
+      expect(subject.changed("foo.css")).to eq(%w(foo.css))
     end
   end
 end
